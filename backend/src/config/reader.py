a60da0f07e59f5c4dfca672be1b788b5138f9ccd
jonizei <joni.koskinen@cs.tamk.fi>
2022-01-17 22:43:05 +0200

    Add feature to create config files

--- backend/src/config/reader.py
+++ backend/src/config/reader.py
@@ -1,3 +1,24 @@
+from os.path import exists
+
+# Name of the web configuration file
+WEB_CONFIG_FILE = "webconfig.txt"
+# Name of the database configuration file
+DB_CONFIG_FILE = "dbconfig.txt"
+
+# Required data for web configuration
+WEB_CONFIG_DATA = {
+    'HOSTNAME' : 'Server host address',
+    'PORT' : 'Server host port'
+}
+
+# Required data for database configuration
+DB_CONFIG_DATA = {
+    'DB_USER' : 'Database username',
+    'DB_PASS' : 'Database password',
+    'DB_HOST' : 'Database host address',
+    'DB_NAME' : 'Database name',
+    'DB_TABLE' : 'Database table name'
+}
 
 # Reads config file and returns it in a
 # dictionary format
@@ -17,3 +38,40 @@ def create_config_dict(data):
         config.update({tokens[0] : tokens[1]})
 
     return config
+
+# Writes configuration data to a file
+def write_config_file(filepath, data):
+    with open(filepath, 'w') as config_file:
+        for key in data.keys:
+            config_file.write(f"%s = %s\n" % (key, data[key]))
+
+# Asks user to fill required configuration data and saves them to files
+# Creates configuration file for a web server
+# Creates configuration file for a database
+def write_config(web_path, db_path):
+    web_data = {}
+    db_data = {}
+
+    print("Web server configuration:")
+    
+    print("Web config:")
+    for key in WEB_CONFIG_DATA.keys:
+        print(f"%s: " % (WEB_CONFIG_DATA[key]))
+        tmp = input()
+        web_data[key] = tmp.trim()
+
+    print("Database config:")
+    for key in DB_CONFIG_DATA.keys:
+        print(f"%s: " % (DB_CONFIG_DATA[key]))
+        tmp = input()
+        db_data[key] = tmp.trim()
+
+    write_config_file(web_path + WEB_CONFIG_FILE, web_data)
+    write_config_file(db_path + DB_CONFIG_FILE, db_data)
+
+# Checks if both configuration files exists
+def config_exists(web_path, db_path):
+    if exists(web_path + WEB_CONFIG_FILE) and exists(db_path + DB_CONFIG_FILE):
+        return True
+
+    return False
--- backend/src/main.py
+++ backend/src/main.py
@@ -1,8 +1,27 @@
+import sys
 import web.webserver as server
+import os
+import config.reader as configreader
 
-# Starts listening on the given port
-def main():
-    server.listen()
+# FILEPATH = path to the src directory
+FILEPATH = os.path.abspath(os.path.dirname(__file__)) + '/'
+# WEB_CONFIG_PATH = path to the web directory inside src directory
+WEB_CONFIG_PATH = FILEPATH + "web/"
+# DB_CONFIG_PATH = path to the db directory inside src directory
+DB_CONFIG_PATH = FILEPATH + "db/"
+
+# Receives commandline arguments and executes commands based on them
+# 'config' = asks user to fill required configuration
+# 'start' = starts to listen for connections
+def main(args):
+    if len(args) == 1:
+        if args[0] == 'config':
+            configreader.write_config(WEB_CONFIG_PATH, DB_CONFIG_PATH)
+        elif args[0] == 'start':
+            if configreader.config_exists(WEB_CONFIG_PATH, DB_CONFIG_PATH):
+                server.listen()
+            else:
+                print("Configuration files missing.")
 
 if __name__ == '__main__':
-    main()
+    main(sys.argv[1:])
